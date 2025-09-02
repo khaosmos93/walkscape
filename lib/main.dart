ada9868199b48972274a8db71bb893c5c3062a51
Minseok Oh <khaosmos93@gmail.com>
2025-09-02 15:49:16 +0900

    basic dependency setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:maplibre_gl/mapbox_gl.dart';
 
-void main() => runApp(const WalkscapE());
+void main() => runApp(const WalkscapeApp());
 
-class WalkscapE extends StatelessWidget {
-  const WalkscapE({super.key});
+class WalkscapeApp extends StatelessWidget {
+  const WalkscapeApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MapPage(),
+    return MaterialApp(
+      title: 'WalkscapE',
+      theme: ThemeData(brightness: Brightness.dark, useMaterial3: true),
+      home: const _Home(),
     );
   }
 }
 
-class MapPage extends StatefulWidget {
-  const MapPage({super.key});
-  @override
-  State<MapPage> createState() => _MapPageState();
-}
-
-class _MapPageState extends State<MapPage> {
-  MapLibreMapController? _controller;
+class _Home extends StatelessWidget {
+  const _Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('WalkscapE')),
-      body: MapLibreMap(
-        styleString: 'https://demotiles.maplibre.org/style.json', // 추후 교체
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(37.5665, 126.9780),
-          zoom: 12,
-        ),
-        myLocationEnabled: true,
-        myLocationTrackingMode: MyLocationTrackingMode.Tracking,
-        onMapCreated: (c) => _controller = c,
-      ),
+    return const Scaffold(
+      body: Center(child: Text('WalkscapE booted ✔')),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import geolocator_apple
+import in_app_purchase_storekit
+import isar_flutter_libs
+import package_info_plus
+import path_provider_foundation
+import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
+  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
